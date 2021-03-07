163a8ef7ec6b0600da1b9552f9bf2ecd893c036b
Nikolay-Sk <69104529+Nikolay-Sk@users.noreply.github.com>
2021-03-07 23:16:05 +0300

    loginModal v.1

---
+++ src/js/loginModal.js
@@ -0,0 +1,40 @@
+const refs = {
+  loginCloseModalBtn: document.querySelector('.js-loginBtnClose'),
+  loginModalBackdrop: document.querySelector('.js-loginBackdrop'),
+  loginSignIn: document.querySelector('.js-SignIn'),
+  // movieWrap: document.querySelector(".movieModalWrap"),
+};
+
+// function generateHTML(data) {
+//   refs.movieWrap.insertAdjacentHTML("beforeend", movieModalTemplate(data));
+// }
+
+refs.loginSignIn.addEventListener('click', onSignInClick);
+refs.loginCloseModalBtn.addEventListener('click', onButtonCloseClick);
+refs.loginModalBackdrop.addEventListener('click', onBackdropClick);
+
+function onButtonCloseClick() {
+  loginToggleModal();
+  // refs.movieWrap.innerHTML = '';
+  window.removeEventListener('keydown', pressEsc);
+}
+function onBackdropClick(event) {
+  if (event.target === event.currentTarget) {
+    onButtonCloseClick();
+  }
+}
+function pressEsc(event) {
+  if (event.code === 'Escape') {
+    onButtonCloseClick();
+  }
+}
+
+function loginToggleModal() {
+  refs.loginModalBackdrop.classList.toggle('is-hidden');
+}
+
+function onSignInClick(event) {
+  event.preventDefault();
+  loginToggleModal();
+  window.addEventListener('keydown', pressEsc);
+}
