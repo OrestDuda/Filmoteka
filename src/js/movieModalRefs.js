b5068edfa3efa1e2ad0be44d5305572b5956d0df
Kateryna Boikova <kateboikova@gmail.com>
2021-03-07 16:08:19 +0100

    adj

---
+++ src/js/movieModalRefs.js
@@ -0,0 +1,8 @@
+const refs = {
+  closeModalBtn: document.querySelector('.movieModalClose'),
+  modalBackdrop: document.querySelector('.js-movieModal'),
+  modalContainer: document.querySelector('.movieModal'),
+  allMovieList: document.querySelector('.products-list'),
+  movieWrap: document.querySelector('.movieModalWrap'),
+};
+export default refs;
