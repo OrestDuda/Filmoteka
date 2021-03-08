362e902f4ce76ceeab2fc6553a347bfc6eeacb17
Orest <Orestduda@gmail.com>
2021-03-08 22:18:26 +0200

    commented

--- src/js/replace_empty_pic.js
+++ src/js/replace_empty_pic.js
@@ -1,11 +1,11 @@
-const refs = {
-  moviePic: () => document.querySelector('.imgFilm'),
-};
-
-function replaceEmptyPic() {
-  const picSource = refs.moviePic().getAttribute('src');
-  if (picSource === './images/film1.jpg') {
-    refs.moviePic().setAttribute('src', '../images/movie_placeholder.svg');
-  }
-}
-replaceEmptyPic();
+// const refs = {
+//   moviePic: () => document.querySelector('.imgFilm'),
+// };
+//
+// function replaceEmptyPic() {
+//   const picSource = refs.moviePic().getAttribute('src');
+//   if (picSource === './images/film1.jpg') {
+//     refs.moviePic().setAttribute('src', '../images/movie_placeholder.svg');
+//   }
+// }
+// replaceEmptyPic();
