8923397ec262f8196b186973c247a05b46e5c1e3
Zinaida Razinkova <razinkova.zina@gmail.com>
2021-03-08 09:32:48 +0200

    Добавляет подгрузку популярных фильмов на главную страницу.

---
+++ src/js/topFilms.js
@@ -0,0 +1,22 @@
+import filmsTpl from '../templates/products.hbs';
+
+const productslistRef = document.querySelector('#products-list-js');
+
+function fetchFilms() {
+  const url = 'https://api.themoviedb.org/3/trending/movie/day';
+  const keyApi = 'cd745b1c38819d91d823e4d3c6c216e8';
+
+  fetch(`${url}?api_key=${keyApi}`)
+    .then(response => response.json())
+    .then(({ results }) => {
+      console.log(results);
+
+      const markup = filmsTpl(results);
+      console.log(markup);
+
+      productslistRef.insertAdjacentHTML('beforeend', markup);
+    })
+    .catch(error => console.log('error'));
+}
+
+fetchFilms();
