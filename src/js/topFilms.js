b4369d65dea9cdf05cc5af8e59fb8bd566c575a2
Zinaida Razinkova <razinkova.zina@gmail.com>
2021-03-08 10:44:30 +0200

    Добавляет фетч по жанрам

--- src/js/topFilms.js
+++ src/js/topFilms.js
@@ -20,3 +20,19 @@ function fetchFilms() {
 }
 
 fetchFilms();
+
+// Жанры
+function getGenres() {
+  const url = 'https://api.themoviedb.org/3/genre/movie/list';
+  const keyApi = 'cd745b1c38819d91d823e4d3c6c216e8';
+
+  fetch(`${url}?api_key=${keyApi}`)
+    .then(response => response.json())
+    .then(({ genres }) => {
+      console.log(genres);
+
+      return genres;
+    })
+    .catch(error => console.log('error'));
+}
+getGenres();
