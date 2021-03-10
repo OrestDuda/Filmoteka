5da212e67724e4f6065575c5b328dfc76e3a5c24
anastasyah86 <nastasyah86@gmail.com>
2021-03-10 20:43:14 +0200

    still doesn't work

--- src/js/change_theme.js
+++ src/js/change_theme.js
@@ -1,28 +1,32 @@
 const refs = {
   themeTogglerLink: () => document.querySelector('#theme-toggler-link'),
   body: () => document.querySelector('body'),
-  movieHeader: () => document.querySelector('.products-name'),
+  movieHeader: () => document.querySelectorAll('.products-name'),
   allTheButtons: () => document.querySelector('button'),
   footer: () => document.querySelector('footer'),
+  footerText: () => document.querySelector('.footer_container'),
+  footerTextTeam: () => document.querySelector('.team__modal'),
+
   theme: localStorage.getItem('theme'),
 };
 
-console.log(refs.themeToggler);
-
+console.dir(refs.themeTogglerLink);
 refs.themeTogglerLink().addEventListener('click', themeToggler());
 
 function themeToggler() {
   refs.body().classList.toggle('dark-background');
-  refs.movieHeader().classList.toggle('white-text');
+  refs.movieHeader().forEach(el => el.classList.toggle('white-text'));
   refs.allTheButtons().classList.toggle('white-text');
   refs.footer().classList.toggle('footer-dark-background');
+  refs.footerText().classList.toggle('white-text');
+  refs.footerTextTeam().classList.toggle('white-text');
 
   //   localStorage.setItem('theme', 'dark-theme');
 }
 
 // function changeThemeIf() {
 //   if (!refs.checkbox.checked && refs.body.classList.contains('dark-theme')) {
-//     refs.body.c  lassList.replace('dark-theme', 'light-theme');
+//     refs.body.classList.replace('dark-theme', 'light-theme');
 //     localStorage.setItem('theme', 'light-theme');
 //   }
 // }
