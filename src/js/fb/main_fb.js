a89e8b11a8bbdfa8277afe1df55e471abf6e705e
TMFV <68297974+TMFV@users.noreply.github.com>
2021-03-12 18:28:30 +0200

    sign out without reload

--- src/js/fb/main_fb.js
+++ src/js/fb/main_fb.js
@@ -88,7 +88,8 @@ refs.signinFormRef.addEventListener('submit', event => {
 //Прослуховування кнопки-посилання та відправка даних для Виходу через signOutUserFn()
 refs.btnSignOutRef.addEventListener('click', event => {
   fbfn.signOutUserFn(curUser);
-  document.location.reload();
+  document.querySelector('.js-userSpan').innerHTML = '';
+  //document.location.reload();
 });
 
 //===============================================================================
